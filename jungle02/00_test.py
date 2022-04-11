2ee6cfa7fec6e19acce5a383779c5977fa4331c8
jocy0412 <jakob1@naver.com>
2022-04-12 00:02:21 +0900

    정글 알고리즘 2주차

--- jungle02/00_test.py
+++ jungle02/00_test.py
@@ -3,8 +3,14 @@ import sys
 #     command = sys.stdin.readline().split()
 # print(command)
 
-a = int(input())
-for i in range(a):
-    b = sys.stdin.readline()
-    s = list(b)
-    print(s)
+# a = int(input())
+# for i in range(a):
+#     b = sys.stdin.readline()
+#     s = list(b)
+#     print(s)
+
+n = int(input())
+
+board = [([0] * n) for _ in range(n)]
+
+print(board)
