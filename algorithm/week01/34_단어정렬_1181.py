259fe237519a52dd7f4a14e79e50823dd0a26c0b
jocy0412 <jakob1@naver.com>
2022-05-06 11:13:06 +0900

    알고리즘 1주차 복습 정리

--- algorithm/week01/34_단어정렬_1181.py
+++ algorithm/week01/34_단어정렬_1181.py
@@ -3,4 +3,11 @@ input = sys.stdin.readline
 
 T = int(input())
 data = [input().strip() for i in range(T)]
+
 print(data)
+print('0번값 길이',len(data[0]))
+print('1번값 길이',len(data[1]))
+print('2번값 길이',len(data[2]))
+
+# set()
+# 중복 제거 후에
